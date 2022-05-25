bf1eb08d88e1bdf73e28586df22c65b759b402ca
Taejoeng Kwak <tjkwak@pntbiz.com>
2022-05-25 18:05:21 +0900

    vms 1차 QA

---
+++ js/index.js
@@ -0,0 +1,170 @@
+
+window.onload = function(){
+    console.log('index')
+    var header_btn_area = document.querySelector('.web-menu');
+    var header_btn = header_btn_area.querySelectorAll('.menu');
+    var header_box = header_btn_area.querySelectorAll('.app-header__menu');
+
+    header_btn_area.addEventListener('click',function(e){
+        e.preventDefault();
+        if (!e.target.tagName == 'A') return;
+
+        if(e.target.className === 'menu active'){
+            for(let i=0; i < header_btn.length; i++){
+                header_btn[i].classList.remove('active');
+                header_box[i].classList.remove('active');
+            }
+        }
+        else{
+            for(let i=0; i < header_btn.length; i++){
+                header_btn[i].classList.remove('active');
+                header_box[i].classList.remove('active');
+            }
+            var currIndex = Array.from(header_btn).indexOf(e.target);
+            e.target.classList.add('active');
+            header_box[currIndex].classList.add('active');
+        }
+    })
+
+    //모든 form-disable 에 disalbed 속성 추가
+    var disable_forms = document.querySelectorAll('#root .form-disable');
+            
+    for(let i=0; i<disable_forms.length; i++){
+        disable_forms[i].disabled = true;
+    }
+
+    //테마변경 기능
+    $('.theme-light').click(function(){
+        $('#root .app-container').attr('class','app-container');
+        $('#root .app-container').addClass('app-theme-pnt-light');
+    })
+    $('.theme-dark').click(function(){
+        $('#root .app-container').attr('class','app-container');
+        $('#root .app-container').addClass('app-theme-pnt-dark');
+    })
+    
+    //헤더 - 시간단위 변경기능
+    var time_btn = document.querySelector('.time-box a');
+    var time_txt = time_btn.querySelector('p');
+    time_btn.addEventListener('click',function(e){
+        e.preventDefault();
+        if (!e.target.tagName == 'A') return;
+
+        time_txt.innerHTML = (time_txt.innerHTML === "<b>AM</b>03:30:01") ?
+        "15:30:01" : "<b>AM</b>03:30:01"
+    })
+
+    //헤더 - 유저박스 드롭다운기능
+    $('.user-box .user-btn').click(function(){
+        $('.user-box .user-btn').toggleClass('active');
+        $('.user-box .user-dropbox').toggleClass('active');
+    })
+
+
+    //모바일 사이드바 오픈
+    var mobile_side_btn = document.querySelector('.mobile-menu button');
+    var mobile_side_box = document.querySelector('.mobile-sideBar');
+
+    mobile_side_btn.addEventListener('click',function(){
+        mobile_side_box.classList.toggle('active');
+        var btn_icon = mobile_side_btn.childNodes[0];
+        btn_icon.className = ( btn_icon.className === 'icon-hamburger') ?
+        'icon-close' : 'icon-hamburger'
+    })
+
+
+    /////////////////////////////////
+    //사이드바
+    var sidebar_btn_area = document.querySelector('.app-sideBar .icon-menu-list');
+    var sidebar_btns = sidebar_btn_area.querySelectorAll('a');
+    var sidebar_boxs = document.querySelectorAll('.app-optionBar__wrap');
+    var sidebar_box_area = document.querySelector('.app-optionBar');
+
+    sidebar_btn_area.addEventListener('click',function(e){
+        e.preventDefault();
+        if (!e.target.tagName == 'A') return;
+
+        //사이드바 완전 접기 기능
+        if(e.target.className === 'active'){
+            sidebar_box_area.classList.add('folded');
+            e.target.className = '';
+        }
+        else{
+            sidebar_box_area.classList.remove('folded');
+
+            //사이드바 다른 메뉴 오픈 기능
+            for(let i=0; i < sidebar_boxs.length; i++){
+                sidebar_btns[i].classList.remove('active');
+                sidebar_boxs[i].classList.remove('active');
+            }
+            var currIndex = Array.from(sidebar_btns).indexOf(e.target);
+            e.target.classList.add('active');
+            sidebar_boxs[currIndex].classList.add('active');
+        }
+    })
+
+    //드롭버튼 기능
+    $('.btn-dropdown').click(function(){
+        $('.btn-dropbox').toggleClass('active')
+    })
+
+    //알람박스 끄고 키는 기능
+    var notice_box = document.querySelector('.notice-box');
+    var notice_close_btn = notice_box.querySelector('a');
+    var notice_open_btn = document.querySelector('.info-menu-list a');
+
+    notice_close_btn.addEventListener('click',function(){
+        notice_box.classList.add('disappear')
+        notice_box.classList.remove('appear')
+    })
+    notice_open_btn.addEventListener('click',function(){
+        notice_box.classList.remove('disappear')
+        notice_box.classList.add('appear')
+    })
+
+
+    //프레임 조절 바
+    $('.app-footer .btn-frame-fold').click(function(){
+        $('.btn-frame-fold').children().attr('class') == 'icon-up icon-small' ?
+        $('.btn-frame-fold').children().attr('class','icon-down icon-small')
+        : $('.btn-frame-fold').children().attr('class','icon-up icon-small')
+        $('.app-container').toggleClass('frame-active');
+    })
+
+    //전체화면 기능
+    $('.btn-fullscreen').click(function(){
+        $('.app-container').addClass('full-screen-active');
+        $('#root .fullsc-box').removeClass('d-none');
+    })
+    $('.fullsc-box a').click(function(){
+        $('.app-container').removeClass('full-screen-active');
+        $('#root .fullsc-box').addClass('d-none');
+    })
+
+    //cctv 즐겨찾기 버튼
+    $('.btn-favorite').click(function(){
+        $('.btn-favorite').children().toggleClass('opacity-03');
+    })
+
+
+    /////////////////////////////////////////
+    //팝업
+    $('.btn-url-input').click(function(){
+        $('#newCamera .v-hidden').removeClass('v-hidden');
+        $('#newCamera .sub-content').removeClass('opacity-05');
+        $('#newCamera .form-disable').removeClass('form-disable');
+    })
+    console.log($('#newVideo .pnt-checkbox input').prop('checked'))
+    $('#newVideo .pnt-checkbox input').click(function(){
+        if($('#newVideo .pnt-checkbox input').prop('checked') == true){
+            console.log('hi')
+            $('#newVideo .form-disable').removeClass('form-disable');
+        }
+        else return;
+    })
+
+
+
+
+}
+
