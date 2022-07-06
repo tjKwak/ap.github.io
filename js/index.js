b57a33dca90d60c0eafda6e4cbfadc052d26f621
Taejoeng Kwak <tjkwak@pntbiz.com>
2022-07-06 13:01:34 +0900

    인도어플러스 2차 업로드

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ var header_btn = header_btn_area.querySelectorAll('.menu');
 var body_menu = document.querySelectorAll('.body-wrap > div');
 var widget_lang = document.querySelector('.widget-lang');
 var widget_theme = document.querySelector('.widget-theme');
-var widget_download = document.querySelector('.widget-download');
+var widget_download = document.querySelector('.widget-info');
+var side_right = document.querySelector('.app-body .side-right');
 
 
 /////////////////////////////////////////////////
@@ -27,14 +28,17 @@ header_btn_area.addEventListener('click',function(e){
     if(e.target.innerText == '홈'){
         widget_download.classList.remove('v-hidden')
         widget_download.classList.remove('opacity-0')
+        side_right.classList.remove('short')
     }
     else if(e.target.innerText == '제품 보기'){
         widget_download.classList.add('v-hidden')
         widget_download.classList.add('opacity-0')
+        side_right.classList.add('short')
     }
     else if(e.target.innerText == '문의'){
         widget_download.classList.remove('v-hidden')
         widget_download.classList.remove('opacity-0')
+        side_right.classList.remove('short')
     }
 })
 
@@ -64,6 +68,7 @@ product_btn_area.addEventListener('click', function(e){
     e.target.parentNode.classList.add('active')
     product_cont.classList.add(currClass);
     changeTxt(currClass);
+    changeLink(currClass);
 })
 
 /////////////////////////////////////////////////
@@ -84,6 +89,7 @@ function contArray(nextIndex) {
         product_cont.classList.add(classgroup[0])
         newActive(0)
         changeTxt(classgroup[0]);
+        changeLink(classgroup[0]);
         return;
     }
     if (currIndex + nextIndex === -1) {
@@ -94,6 +100,7 @@ function contArray(nextIndex) {
     product_cont.classList.add(classgroup[currIndex + nextIndex])
     newActive(currIndex + nextIndex)
     changeTxt(classgroup[currIndex + nextIndex]);
+    changeLink(classgroup[currIndex + nextIndex]);
 }
 const newActive = (index) => {
     for(let i=0; i<product_btn.length; i++){
@@ -111,6 +118,18 @@ btn_prev.addEventListener('click',function(){
     contArray(-1);
 })
 
+///////////////////////////////////////////////
+//프로덕트 링크 기능
+// product_img.addEventListener('click',function(e){
+//     console.log(e.target)
+
+//     if(e.target.className === 'rms'){
+//         window.location.href = 'https://naver.com';
+//     }
+//     console.log('hi')
+// })
+
+
 
 ///////////////////////////////////////////////
 //내용 변환 기능
@@ -118,28 +137,37 @@ const productObject = {
     rms: {
         title: 'RMS',
         subTitle: 'RTLS Managing System',
-        desc: 'WMS 솔루션의 새로운 모니터링 솔루션은<br>정해진 메뉴에만 구분하지 않고 자유롭게 커스텀하는 메뉴들을 구성합니다.<br>고객 요구사항에 맞는 맞춤형 대시보드 RMS를 소개합니다.'
+        desc: 'WMS 솔루션의 새로운 모니터링 솔루션은<br>정해진 메뉴에만 구분하지 않고 자유롭게 커스텀하는 메뉴들을 구성합니다.<br>고객 요구사항에 맞는 맞춤형 대시보드 RMS를 소개합니다.',
+        link: 'https://asia1-rms.indoorplus.io'
     },
     at: {
         title: 'AT',
         subTitle: 'Asset Tracking',
-        desc: '위치기반 IoT 자산 솔루션은 소중한 모든것을 추적합니다.<br>의료기기의 위치뿐만 아니라 각종 상태와 정보까지<br>확인 가능한 모든걸 연결 분석하는 자산 추적 솔루션입니다.'
+        desc: '위치기반 IoT 자산 솔루션은 소중한 모든것을 추적합니다.<br>의료기기의 위치뿐만 아니라 각종 상태와 정보까지<br>확인 가능한 모든걸 연결 분석하는 자산 추적 솔루션입니다.',
+        link: 'https://asia1-at.indoorplus.io'
     },
     am: {
         title: 'AM',
         subTitle: 'Asset Management',
-        desc: '고객의 기기,장비,물류 등의 위치와 상태를 시각화,통계화합니다.<br>특정 자산의 조치와 자산 운용 및 플로우 현상을 위해 관리자가 취해야 할 행동반경을 제시합니다.<br>효율적 활용, 안정성 제고, 운영/관리 효율화를 위한 자산관리 시스템 솔루션입니다.'
+        desc: '고객의 기기,장비,물류 등의 위치와 상태를 시각화,통계화합니다.<br>특정 자산의 조치와 자산 운용 및 플로우 현상을 위해 관리자가 취해야 할 행동반경을 제시합니다.<br>효율적 활용, 안정성 제고, 운영/관리 효율화를 위한 자산관리 시스템 솔루션입니다.',
+        link: 'https://asia1-am.indoorplus.io'
     },
     ws: {
         title: 'WS',
         subTitle: 'Worker Safety',
-        desc: '사고의 예방과 조치를 위해 가장 중요한 것은 작업자의 실시간 위치 파악입니다.<br>작업자 안전 관리솔루션은 작업자의 위치 및 생체데이터를 모니터링 하여,<br>안전 관리자에게 시설 현황 파악 및 구역별 인원 관리를 돕습니다.'
+        desc: '사고의 예방과 조치를 위해 가장 중요한 것은 작업자의 실시간 위치 파악입니다.<br>작업자 안전 관리솔루션은 작업자의 위치 및 생체데이터를 모니터링 하여,<br>안전 관리자에게 시설 현황 파악 및 구역별 인원 관리를 돕습니다.',
+        link: 'https://asia1-ws.indoorplus.io'
     },
     sh: {
         title: 'SH',
         subTitle: 'Smart Hospital',
-        desc: '환자가 편안한 병원, 환자가 안전한 병원<br>치료 공백이 없는 병원, 의료진이 편리한 병원<br>IoMT 스마트 병원 솔루션이 만들어 갑니다.'
+        desc: '환자가 편안한 병원, 환자가 안전한 병원<br>치료 공백이 없는 병원, 의료진이 편리한 병원<br>IoMT 스마트 병원 솔루션이 만들어 갑니다.',
+        link: 'https://asia1-sh.indoorplus.io'
     }
+}
+function changeLink(productName){
+    const product = productObject[productName];
+    product_img.parentElement.href = product.link
 }
 
 function changeTxt(productName){
